a7289b695d89bf17c2ed7e1524d86e7e846c80a6
gideon <gideonowusu2@gmail.com>
2023-07-26 11:54:46 +0000

    added image to try branch

--- web.js
+++ web.js
@@ -3,6 +3,6 @@ let username;
 document.getElementById("mybutton").onclick=function(){
     username=document.getElementById("mytext").value;
     console.log(username);
-    document.getElementById("mylable").innerHTML= "HELLO "+ username;
+    document.getElementById("mylable").innerHTML= "HELLO "+ username +" Welcome to GOA Assistance";
 }
 
