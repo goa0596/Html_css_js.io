22b5864bde6376a776aee038b032d842180e3ff9
gideon <gideonowusu2@gmail.com>
2023-07-26 00:23:57 +0000

    my first interactive html js

---
+++ web.js
@@ -0,0 +1,8 @@
+//let name= window.prompt("whats username"); 
+let username;
+document.getElementById("mybutton").onclick=function(){
+    username=document.getElementById("mytext").value;
+    console.log(username);
+    document.getElementById("mylable").innerHTML= "HELLO "+ username;
+}
+
