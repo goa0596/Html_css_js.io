865863680875b02837db6112fa3e3c143f2d6a86
gideon <gideonowusu2@gmail.com>
2023-07-29 16:12:59 +0000

    different approach. A way forward

--- web.js
+++ web.js
@@ -1,8 +1,12 @@
 //let name= window.prompt("whats username"); 
 let username;
-document.getElementById("mybutton").onclick=function(){
+function disp(){
     username=document.getElementById("mytext").value;
+    username= username.toUpperCase();
     console.log(username);
     document.getElementById("mylable").innerHTML= "HELLO "+ username +" Welcome to GOA Assistance";
+    document.getElementById("demo").innerHTML="HELLO, " + username  + " WELCOME TO MY PAGE";
+    //password = window.prompt("PassWord")
+    //console.log(password)
 }
 
